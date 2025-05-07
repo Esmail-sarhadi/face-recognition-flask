20177c31dd616cf85725dcf3069259c782377feb
Esmail Sarhadi <45729949+Esmail-sarhadi@users.noreply.github.com>
2025-05-07 08:24:44 +0330

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'known_faces'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
-
+ 
 # اطمینان از وجود پوشه known_faces
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
